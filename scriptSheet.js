d18c9eb715fbf5fa78d213ea0d1d2cbe735b292d
berry-kae <knwhitener@uncg.edu>
2026-04-09 13:26:54 -0400

    added more elements

--- scriptSheet.js
+++ scriptSheet.js
@@ -1,10 +1,11 @@
 function spawnButton(){
         const input = document.getElementById('inputbox1');
-        const btn = document.getElementById('button4');
-        if(input.value.trim().length >7){
+        const btn = document.getElementById('button1');
+        if(input.value.trim().length >4){
             btn.style.display='inline';
         } else{
             btn.style.display = 'none';
         }
         
     }
+    
