e8262d9da56ad90f8663c2c70de8d9c20647d96e
berry-kae <knwhitener@uncg.edu>
2026-04-13 21:39:49 -0400

    added more elements to prototype page

--- scriptSheet.js
+++ scriptSheet.js
@@ -1,11 +1,3 @@
-function spawnButton(){
-        const input = document.getElementById('inputbox1');
-        const btn = document.getElementById('button1');
-        if(input.value.trim().length >4){
-            btn.style.display='inline';
-        } else{
-            btn.style.display = 'none';
-        }
-        
-    }
-    
+document.getElementById('hamster').onclick = function() {
+    document.getElementById('button2');
+};
